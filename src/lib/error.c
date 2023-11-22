f633386133d0c5371a0d31be03847ae2821cb3d3
kevinchar93 <kevincharles018@gmail.com>
2023-11-22 23:39:54 +0000

    update error string & correct buh causing get_jsonfmt_error_string to always return NULL

--- src/lib/error.c
+++ src/lib/error.c
@@ -3,22 +3,22 @@
 const char *const jsfmt_error_strings[] =
     {
         "no error",
-        "can't use flags set at the same time: %s",
+        "can't use flags set at the same time: '%s'",
         "can't use --write(-w) with no set 'path'",
-        "unrecognised option: %s",
-        "incorrect type for arg: %s",
-        "option appears more than once: %",
-        "expected value for option: %s",
-        "value provided for option ('%s') is out of range: (%s)",
+        "unrecognised option: '%s'",
+        "incorrect type for arg: '%s'",
+        "option appears more than once: '%s'",
+        "expected value for option: '%s'",
+        "value provided for option ('%s') is out of range: %s",
         "permission denied reading path '%s'",
         "permission denied writing path '%s'",
-        "path does not exist: %s",
+        "path does not exist: '%s'",
         "no .json files found in input path(s)",
-        "path is resolved to be null: %s",
-        "i/o error occurred resolving path: %s",
-        "too many symlinks resolving path: %s",
-        "path is too long to resolve: %s ",
-        "prefix in path is not a directory: %s",
+        "path is resolved to be null: '%s'",
+        "i/o error occurred resolving path: '%s'",
+        "too many symlinks resolving path: '%s'",
+        "path is too long to resolve: '%s'",
+        "prefix in path is not a directory: '%s'",
     };
 
 
@@ -27,15 +27,12 @@ _Static_assert(ARRAY_LEN(jsfmt_error_strings) == JSONFMT_ERR_COUNT,
                "`jsfmt_error_strings` array in-sync.");
 
 
-const char* get_jsonfmt_error_string(jsonfmt_error_t error_code)
-{
-  const char* err_str = NULL;
+const char *get_jsonfmt_error_string(jsonfmt_error_t error_code) {
 
-  if (error_code > JSONFMT_OK && error_code < JSONFMT_ERR_COUNT )
-  {
-    return NULL;
+  if (error_code > JSONFMT_OK &&
+      error_code < JSONFMT_ERR_COUNT) {
+    return jsfmt_error_strings[error_code];
   }
 
-  err_str = jsfmt_error_strings[error_code];
-  return err_str;
+  return NULL;
 }
